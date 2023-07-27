682f3d01a3b14bc79b5c93a7738dd67e203e5c02
Peter Bakkum <pbbakkum@gmail.com>
2023-07-27 01:07:59 -0700

    Experimenting with method parsing

--- main.go
+++ main.go
@@ -43,6 +43,31 @@ func (this *ParsedFunction) String() string {
 	return str.String()
 }
 
+type ParsedMethod struct {
+	Name          string
+	Params        string
+	ReturnType    string
+	Documentation string
+	Range         *sitter.Range
+	Node          *sitter.Node
+}
+
+func (this *ParsedMethod) Testxxx(a int, b bool, c ...string) (bool, error) {
+	return true, nil
+}
+
+func (this *ParsedMethod) String() string {
+	var str strings.Builder
+	if this.Documentation != "" {
+		str.WriteString(this.Documentation)
+		str.WriteString("\n")
+	}
+	str.WriteString(fmt.Sprintf("%s%s %s", this.Name, this.Params, this.ReturnType))
+	str.WriteString(" ")
+	str.WriteString(RangeString(this.Range))
+	return str.String()
+}
+
 type ParsedTypeDefinition struct {
 	Definition    string
 	Documentation string
@@ -80,6 +105,7 @@ func GetRange(node *sitter.Node) *sitter.Range {
 type ParsedSymbol struct {
 	Function       *ParsedFunction
 	TypeDefinition *ParsedTypeDefinition
+	Method         *ParsedMethod
 }
 
 func (this *ParsedSymbol) String() string {
@@ -87,6 +113,8 @@ func (this *ParsedSymbol) String() string {
 		return this.TypeDefinition.String()
 	} else if this.Function != nil {
 		return this.Function.String()
+	} else if this.Method != nil {
+		return this.Method.String()
 	}
 	return ""
 }
@@ -97,7 +125,15 @@ func precedingComments(node *sitter.Node, sourceCode []byte) (string, *sitter.Po
 	var startPoint *sitter.Point
 	var startByte uint32
 
-	cursor := sitter.NewTreeCursor(node.Parent())
+	if node == nil {
+		panic("node is nil")
+	}
+	parent := node.Parent()
+	if parent == nil {
+		return "", nil, 0
+	}
+
+	cursor := sitter.NewTreeCursor(parent)
 	cursor.GoToFirstChild()
 
 	// Go through all siblings
@@ -188,6 +224,159 @@ func QueryFunctions(rootNode *sitter.Node, lang *sitter.Language, sourceCode []b
 	return funcs, nil
 }
 
+func QueryMethods2(rootNode *sitter.Node, lang *sitter.Language, sourceCode []byte) ([]*ParsedMethod, error) {
+	// Query for method definitions
+	pattern := `(method_declaration
+		(parameter_list) @receiver
+		(field_identifier) @method.name
+		(parameter_list) @method.parameters
+		(type_identifier) @method.type)`
+
+	// Execute the query
+	query, err := sitter.NewQuery([]byte(pattern), lang)
+	if err != nil {
+		return nil, err
+	}
+	queryCursor := sitter.NewQueryCursor()
+	queryCursor.Exec(query, rootNode)
+
+	methods := []*ParsedMethod{}
+
+	// Iterate over query results
+	for {
+		match, ok := queryCursor.NextMatch()
+		if !ok {
+			break
+		}
+		fmt.Printf("match: %v\n", match)
+
+		for _, cap := range match.Captures {
+			fmt.Printf("cap: %s\n", string(cap.Node.Content(sourceCode)))
+		}
+	}
+
+	return methods, nil
+}
+
+func QueryMethods3(rootNode *sitter.Node, lang *sitter.Language, sourceCode []byte) ([]*ParsedMethod, error) {
+	// Query for method definitions
+	pattern := `(method_declaration) @method`
+
+	// Execute the query
+	query, err := sitter.NewQuery([]byte(pattern), lang)
+	if err != nil {
+		return nil, err
+	}
+	queryCursor := sitter.NewQueryCursor()
+	queryCursor.Exec(query, rootNode)
+
+	methods := []*ParsedMethod{}
+
+	// Iterate over query results
+	for {
+		match, ok := queryCursor.NextMatch()
+		if !ok {
+			break
+		}
+		fmt.Printf("match: %v\n", match)
+
+		for _, cap := range match.Captures {
+			// iterate over capture children
+			cursor := sitter.NewTreeCursor(cap.Node)
+			cursor.GoToFirstChild()
+
+			code := ""
+
+			for {
+				currNode := cursor.CurrentNode()
+				//fmt.Printf("child: %s %s\n", currNode.Type(), cursor.CurrentFieldName())
+				if cursor.CurrentFieldName() != "body" {
+					fmt.Printf("child: %s %s %s\n", currNode.Type(), cursor.CurrentFieldName(), string(currNode.Content(sourceCode)))
+					code += string(currNode.Content(sourceCode))
+				}
+
+				ok = cursor.GoToNextSibling()
+				if !ok {
+					break
+				}
+			}
+
+			fmt.Printf("code: %s\n", code)
+		}
+	}
+
+	return methods, nil
+}
+
+// Identify method declarations
+func QueryMethods(rootNode *sitter.Node, lang *sitter.Language, sourceCode []byte) ([]*ParsedMethod, error) {
+	// Query for method definitions
+	pattern := "(method_declaration) @method.name"
+	// (method_declaration
+	//   name: (identifier) @method.name
+	//   type: (function_type
+	//     parameters: (field_list
+	//       (field_declaration
+	//         name: (identifier) @parameter.name
+	//         type: (_) @parameter.type))))
+
+	// Execute the query
+	query, err := sitter.NewQuery([]byte(pattern), lang)
+	if err != nil {
+		return nil, err
+	}
+	queryCursor := sitter.NewQueryCursor()
+	queryCursor.Exec(query, rootNode)
+
+	methods := []*ParsedMethod{}
+
+	// Iterate over query results
+	for {
+		match, ok := queryCursor.NextMatch()
+		if !ok {
+			break
+		}
+
+		for _, cap := range match.Captures {
+			fmt.Printf("cap: %v\n", cap)
+			switch name := query.CaptureNameForId(cap.Index); name {
+			case "method.name":
+				node := cap.Node.Parent()
+				rng := GetRange(node)
+
+				newMethod := ParsedMethod{
+					Name:  string(cap.Node.Content(sourceCode)),
+					Node:  node,
+					Range: rng,
+				}
+				log.Printf("newMethod: %v\n", newMethod)
+
+				paramList := node.ChildByFieldName("parameters")
+				log.Printf("paramList: %v\n", paramList)
+				if paramList != nil {
+					newMethod.Params = string(paramList.Content(sourceCode))
+				} else {
+					newMethod.Params = "()"
+				}
+				returnType := node.ChildByFieldName("result")
+				log.Printf("returnType: %v\n", returnType)
+				if returnType != nil {
+					newMethod.ReturnType = string(returnType.Content(sourceCode))
+				}
+				doc, commentStart, commentStartBytes := precedingComments(node, sourceCode)
+				if doc != "" {
+					newMethod.Documentation = doc
+					newMethod.Range.StartPoint = *commentStart
+					newMethod.Range.StartByte = commentStartBytes
+				}
+				methods = append(methods, &newMethod)
+			}
+		}
+	}
+
+	return methods, nil
+}
+
 func QueryTypeDefinitions(rootNode *sitter.Node, lang *sitter.Language, sourceCode []byte) ([]*ParsedTypeDefinition, error) {
 	// Query for type definitions
 	pattern := "(type_spec (type_identifier)) @type.name"
@@ -258,6 +447,18 @@ func QuerySymbols(rootNode *sitter.Node, lang *sitter.Language, sourceCode []byt
 	for _, t := range types {
 		symbol := ParsedSymbol{
 			TypeDefinition: t,
+		}
+		symbols = append(symbols, &symbol)
+	}
+
+	methods, err := QueryMethods3(rootNode, lang, sourceCode)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, method := range methods {
+		symbol := ParsedSymbol{
+			Method: method,
 		}
 		symbols = append(symbols, &symbol)
 	}
